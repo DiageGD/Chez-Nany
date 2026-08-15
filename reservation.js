a76a9141118f1b929eed33063c0b0f4b5bb7e925
tiagodasilva-dev <159145332+DiageGD@users.noreply.github.com>
2026-08-15 11:16:18 +0200

    Modify reservation logic and alert for maintenance
    Updated alert message and added maintenance notice. Adjusted reservation logic to prevent bookings during maintenance period.

--- reservation.js
+++ reservation.js
@@ -1,4 +1,4 @@
-alert("Le restaurant est fermé et ne réouvrira ses portes que le 3 septembre, merci de ne pas réserver de table pour avant cette date (ou la réservation sera supprimée).")
+alert("Le restaurant est fermé et ne réouvrira ses portes que le 3 septembre, merci de ne pas réserver de table pour avant cette date (ou la réservation sera supprimée). De plus le site est actuellement en maintenance pour plus de sécurité pour vos données, aucune réservation n'est actuellement faisable.")
 document.addEventListener("DOMContentLoaded", async () => {
   const supabase = window.supabaseClient || window.supabase || supabaseClient;
   if (!supabase) return;
